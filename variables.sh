eecff924068ff3fda138427149fdcea3d29eb5d5
chakra <chakradhar06@gmail.com>
2022-03-14 02:35:27 -0400

    Variable declaration

--- variables.sh
+++ variables.sh
@@ -6,5 +6,14 @@ echo ${a}times
 echo $b
 
 #{} are needed if variable is combined with other variables without space
+
+DATE=2202-03-12
+echo Today date is $DATE
+
 DATE=$(date +%F)
 echo Today date is $DATE
+
+x=10
+y=20
+ADD=$(($x+$y))
+echo ADD = $ADD
