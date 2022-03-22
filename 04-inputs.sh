2a990008f27a53b9e8e67410f0135f085b6601da
chakra <chakradhar06@gmail.com>
2022-03-22 19:04:52 -0400

    Added input script

---
+++ 04-inputs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p 'Enter you name: ' name
+echo "You name = $name"
+
+echo "what is your name?"
+read name
+echo "My name is $name"
+read remark
+echo "$name , that is good name"
