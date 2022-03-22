cacf69f0ca2f2580b67270949988194420d4e3fd
chakra <chakradhar06@gmail.com>
2022-03-22 19:10:21 -0400

    Added input script

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -8,3 +8,11 @@ read name
 echo "My name is $name"
 read remark
 echo "$name , that is good name"
+
+#We can not use the "Read" in automation as it is prompting for input
+#Special variables are used for input
+# $0-n, $*, . $@, $#
+echo Script name = $0
+echo First argument = $1
+echo All arguments = $*
+echo Number of Arguments = $#
