c3e735f56d31b110fa5774d78dcd10771abe775e
chakra <chakradhar06@gmail.com>
2022-03-22 18:53:36 -0400

    Variables, arrays

--- 03-variables.sh
+++ 03-variables.sh
@@ -40,5 +40,4 @@ echo All values of arry   = ${c[*]}
 #By default variable is readwrite, we can change it to readonly
 #Varaibles on shell are local, to use it in script we need to set it as env using export
 
-Training = ${TRAINING}
-echo $Training
+echo Training = ${TRAINING}
