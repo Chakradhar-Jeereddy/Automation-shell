bf018eefc0bed9aa3620e13ef3876dc8325b2eab
chakra <chakradhar06@gmail.com>
2022-03-22 18:31:10 -0400

    Updated script for variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -15,3 +15,13 @@ echo $b Training
 
 DATE=2022-03-22
 echo Today data is $DATE
+
+#As above all the time we will not hardcode the values of variables and we need the data
+#dynamically, in that case we use command or arithmetic substitution.
+#var = $(command), the command output will be stored in variable
+
+#var=$((expresssion)), this is arithmetic subst, output goes to variable. example is $((2+3))
+
+Date=date
+echo Todays data is $($Date)
+echo a is $((a*2))
