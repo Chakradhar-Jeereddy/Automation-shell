cd562c602d45bd7de06bdc0c8d88e01b6f056c00
chakra <chakradhar06@gmail.com>
2022-03-22 19:23:01 -0400

    Quotes concept

--- 05-quotes.sh
+++ 05-quotes.sh
@@ -4,3 +4,10 @@
 echo Apple cost is $100
 #TO disable special character using single quotes
 echo 'Apple cost is $100'
+#Double quotes will not disable special character
+echo "Apple cost is $100"
+#Uase back word slash \ to disable special character inside double quotes
+echo "Apple cost is \$100"
+Training = Devops
+echo 'Im in ${Training} training'
+echo "Im in ${Training} training"
