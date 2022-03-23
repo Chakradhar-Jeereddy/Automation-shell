29587b75a14fafc8ea4787239315580b55edba4c
chakra <chakradhar06@gmail.com>
2022-03-23 16:36:31 -0400

    condition test

--- roboshop/07-exit-status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Exit status is a number, $? is a special variable that holds this value
-# $? will store the exit status of the last executed command
-#Range of exit status is 0-255
-# 0 - Global success
-# 1-125 -> Some failure from the script/command
-# 125+ -> system failure
-# example lss - command not found with error 127
-
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#issues
+#1. Installation failed, but my script still continued
+#2. Installation failed, because I am not a root user
+#3. Installation failed, because I have not validated that I have root privileges
+#4. The information I would like to provide if success or failure
+#example change the zip to zp in curl command and run the script.
+#5 Any step failed, but my script continued
+#6 Repetitive code is there, in order to deal with this I need to use functions/methods
+
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
   echo -e "\e[36m You should run the script as sudo or root user. \e[0m"
@@ -52,12 +61,6 @@ fi
 
 
 
-#issues
-#1. Installation failed, but my script still continued
-#2. Installation failed, because I am not a root user
-#3. Installation failed, because I have not validated that I have root privileges
-#4. The information I would like to provide if success or failure
-#example change the zip to zp in curl command and run the script.
-#5 Any step failed, but my script continued
+
 
 
