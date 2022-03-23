353f2f1e33ebbfb27419bc7f1e8b970e18123ced
chakra <chakradhar06@gmail.com>
2022-03-23 10:48:44 -0400

    preparing frontend project

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
-#To Install Nginx.
 
-echo -e "\e[31mInstall NGINX package\e[0m"
 yum install nginx -y
 
 
-echo -e "\e[33mDownload frontend files\e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
-echo -e "\e[33mCleanup files\e[0m"
-rm -rf /usr/share/nginx/html/
-cd /usr/share/nginx/html
-
-echo $?
-if [$? -ge 0]; then
-  echo "download failed"
-
-
-echo -e "\e[32mUnarchive files\e[0m"
+rm -rf /usr/share/nginx/html/*
+cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[32mStart nginx\e[0m"
+systemctl restart nginx
 systemctl enable nginx
-systemctl start nginx
-  exit
-fi
