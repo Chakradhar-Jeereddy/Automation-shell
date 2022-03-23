68a0512762883450bab007130280b6cf988b44c4
chakra <chakradhar06@gmail.com>
2022-03-23 18:21:26 -0400

    Function test

---
+++ 09-redirectors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#1> is to write output to file
+#1>> is to append output to file
+#2> is to write errors to file
+#2>> is to append errors to file
+# &> or &>> is to write/append both output and error to file
+# >>/dell/null do not want to store output anywhere
+
+#Concept of alias
+#gp alias = 'git pull &>>/dev/null
