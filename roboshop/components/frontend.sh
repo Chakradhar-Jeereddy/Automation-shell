47712edb606a396c40bbfc5cf37b26d2db23f029
chakra <chakradhar06@gmail.com>
2022-03-23 11:22:00 -0400

    preparing frontend project

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo -e "\e[36m Installing nginx \e[0m"
 yum install nginx -y
 
-
+echo -e "\e[36m Downloading nginx content \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
+echo -e "\e[36m Cleanup old nginx content and unarchive new content \e[0m"
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
@@ -13,6 +15,7 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+echo -e "\e[36m starting nginx \e[0m"
 systemctl restart nginx
 systemctl enable nginx
 
@@ -20,6 +23,6 @@ systemctl enable nginx
 #issues
 #1. Installation failed, but my script still continued
 #2. Installation failed, because I am not a root user
-#3. Installation failed, becuase I have not validated that I have root previleges
+#3. Installation failed, because I have not validated that I have root previleges
 
 
