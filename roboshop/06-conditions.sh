85ca0c5da2531e341389a7e6ea7575c388812ed9
chakra <chakradhar06@gmail.com>
2022-03-23 12:32:08 -0400

    preparing frontend project

---
+++ roboshop/06-conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# case and if conditional commands , if command is widely used because it has more options than case
+# command
+
+#IF condition
+#IF is found in three forms
+
+#Simple if
+# if [expression]
+#then
+# commands
+#fi
+
+if [1 -eg 1];
+ then echo hello
+fi
