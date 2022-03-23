afddbe2f17c78c64d3f06cb66e43c34cf28270e0
chakra <chakradhar06@gmail.com>
2022-03-23 11:10:18 -0400

    preparing frontend project

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -15,3 +15,11 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx
 systemctl enable nginx
+
+...
+issues
+1. Installation failed, but my script still continued
+2. Installation failed, because I am not a root user
+3. Installation failed, becuase I have not validated that I have root previleges
+...
+
