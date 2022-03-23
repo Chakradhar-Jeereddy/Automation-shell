8f8286507373d5a31f01ef31138fb7ea2e7871c8
chakra <chakradhar06@gmail.com>
2022-03-23 16:45:57 -0400

    condition test

---
+++ 08-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Variable : If we assign a name to set of data that is variable
+# Function : If we assign a name to set of commands that is called function
+
+#Syntax
+#function_name() {
+#command1
+#command2
+#}
+
+#To access a function we use
+#funtion_name
+
+#Declare a function
+
+Print_message() {
+  echo Hello
+  echo Good morning
+  echo Welcome to DevOps Training
+}
+
+Print_message
+
+## Note, Function should always be declared first and than you can call the function later in the code
