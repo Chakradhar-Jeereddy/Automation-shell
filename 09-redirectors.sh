313ddfddb156e2f038f7d39e268f6e0ff7be284d
chakra <chakradhar06@gmail.com>
2022-03-23 20:40:05 -0400

    Function test

--- 09-redirectors.sh
+++ 09-redirectors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 #1> is to write output to file
 #1>> is to append output to file
 #2> is to write errors to file
@@ -7,6 +7,7 @@
 # &> or &>> is to write/append both output and error to file
 # >>/dell/null If we do not need any kind of output or error to a file for future reference we try to
 #nullify the output with the help of /dev/null file
-
+#
 #Concept of alias
 #alias gp='git pull &>>/dev/null
+#TO COMMENT ALL LINES IN INTELIJ USE CONTROL + /
