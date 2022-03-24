bfb59abcf8476f74590273af9d28b34412bfdda1
chakra <chakradhar06@gmail.com>
2022-03-24 12:57:43 -0400

    Preparing mongodb

---
+++ 11-loops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Loops are two types
+#Loop based done condition/expression, while loop command
+#Loop based done inputs, for loop command
+
+i=10
+while [ $i -gt 0 ]; do
+  echo Iteration is $i
+  i=$(($i -1))
+done
+
+for name in chakra ravi rama; do
+  echo My name is $name
+done
