fdbf2e0c40ed6e3338222bff1011bd25ef63e5de
chakra <chakradhar06@gmail.com>
2022-03-23 22:19:58 -0400

    Preparing mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -5,30 +5,48 @@ if [ ${USER_ID} -ne 0 ]; then
   exit 1
 fi
 
-echo -e "\e[36m Cleanup existing  mongodb content \e[0m"
+print() {
+  echo -e "\e[36m $1 \e[0m"
+}
+
+StatCheck() {
+  if [ 0 -eq $1 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[32mFAILURE\e[0m"
+    exit 2
+  fi
+}
+print "Cleanup existing  mongodb content"
 rm -rf /tmp/mongodb*
+StatCheck $?
 
-echo -e "\e[36m Downloading mongodb repository \e[0m"
+print "Downloading mongodb repository"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+StatCheck $?
 
-echo -e "\e[36m Installing mongodb \e[0m"
+print "Installing mongodb"
 yum install -y mongodb-org
+StatCheck $?
 
 #1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
 #
 #Config file: `/etc/mongod.conf`
 
-echo -e "\e[36m Starting mongodb \e[0m"
+print "Starting mongodb"
 systemctl start mongod
 systemctl enable mongod
+StatCheck $?
 
-echo -e "\e[36m Downloading schemas of mongodb \e[0m"
+print "Downloading schemas of mongodb"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
 
-echo -e "\e[36m Extracting schema files \e[0m"
+print "Extracting schema files"
 unzip /tmp/mongodb.zip
+StatCheck $?
 
-echo -e "\e[36m Loading schemas into database \e[0m"
+print "Loading schemas into the database"
 mongo < mongodb-main/catalogue.js
 mongo < mongodb-main/users.js
+StatCheck $?
