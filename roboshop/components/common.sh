95ff293a63811de0fc43b35071585bd77cd125e5
chakra <chakradhar06@gmail.com>
2022-03-24 21:02:40 -0400

    Preparing mongodb

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -91,3 +91,14 @@ NODEJS() {
 
   SERVICE_SETUP
 }
+
+MAVEN() {
+  Print "Install Maven"
+  yum install maven -y &>>$LOG_FILE
+  StatCheck $?
+  APP_SETUP
+  Print "Extracting Jar file"
+  cd shipping && mvn clean package >>$LOG_FILE && mv target/shipping-1.0.jar shipping.jar >>$LOG_FILE
+  StatCheck $?
+  SERVICE_SETUP
+}
--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -2,16 +2,4 @@
 
 source components/common.sh
 COMPONENT=shipping
-
-Print "Install Maven"
-yum install maven -y &>>$LOG_FILE
-StatCheck $?
-
-APP_SETUP
-
-Print "Extracting Jar file"
-cd shipping && mvn clean package >>$LOG_FILE && mv target/shipping-1.0.jar shipping.jar >>$LOG_FILE
-
-
-
-SERVICE_SETUP
+MAVEN
