2b394e633153dc1acc58682eac435b6b8fbf7893
chakra <chakradhar06@gmail.com>
2022-03-24 21:57:25 -0400

    Preparing mongodb

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -4,12 +4,12 @@ source components/common.sh
 COMPONENT=payment
 
 Print "Installing Python 3"
-yum install python36 gcc python3-devel -y $>>${LOG_FILE}
+yum install python36 gcc python3-devel -y &>>${LOG_FILE}
 StatCheck $?
 
 APP_SETUP
 
 Print "Install dependencies"
-cd /home/roboshop/payment $>>${LOG_FILE} && pip3 install -r requirements.txt  $>>${LOG_FILE}
+cd /home/roboshop/payment &>>${LOG_FILE} && pip3 install -r requirements.txt  &>>${LOG_FILE}
 
 SERVICE_SETUP
