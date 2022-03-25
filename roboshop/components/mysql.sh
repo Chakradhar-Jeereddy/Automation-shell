ee8441ada47a5479905328da92155180346178c8
chakra <chakradhar06@gmail.com>
2022-03-24 20:18:54 -0400

    Preparing mongodb

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -14,16 +14,31 @@ Print "Starting Mysql"
 systemctl enable mysqld  &>>$LOG_FILE && systemctl start mysqld  &>>$LOG_FILE
 StatCheck $?
 
-#grep temp /var/log/mysqld.log
+echo 'show databases' | mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
+if [ $? -ne 0 ]; then
+  Print "Change Default root password"
+  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" &>>/tmp/rootpass.sql
+  DEFAULT_ROOT_PASSWORD=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
+  StatCheck $?
+fi
 
-#mysql_secure_installation
+echo show plugins | mysql -uroot -pRoboShop@1 2>>${LOG_FILE} | grep validate_password &>>${LOG_FILE}
+if [ $? -eq 0 ]; then
+  Print "Uninstall Password Validate Plugin"
+  echo 'uninstall plugin validate_password;' >/tmp/pass-validate.sql
+  mysql --connect-expired-password -uroot -pRoboShop@1 </tmp/pass-validate.sql  &>>${LOG_FILE}
+  StatCheck $?
+fi
 
-#mysql -uroot -pRoboShop@1
+Print "Downloading schema"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG_FILE}
+StatCheck $?
 
-#curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+Print "Extract Schema"
+cd /tmp && unzip -o  mysql.zip &>>${LOG_FILE}
+StatCheck $?
 
-
-# cd /tmp
-# unzip mysql.zip
-# cd mysql-main
-# mysql -u root -pRoboShop@1 <shipping.sql
+Print "Loading schema"
+cd mysql-main && mysql -u root -pRoboShop@1 <shipping.sql &>>${LOG_FILE}
+StatCheck $?
