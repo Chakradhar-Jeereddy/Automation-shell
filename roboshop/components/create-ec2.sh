f0f4c2b85803e64c3d1e8924434fd0003d3115db
chakra <chakradhar06@gmail.com>
2022-03-27 18:38:51 -0400

    Preparing mongodb

--- roboshop/components/create-ec2.sh
+++ roboshop/components/create-ec2.sh
@@ -27,7 +27,7 @@ create_ec2() {
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]" \
     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
     --security-group-ids $SG_ID | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
-  sed -e "s/component/${COMPONENT}/" -e "s/PRIVATE_IP/${PRIVATE_IP}" dnsrecord.json >/tmp/record.json
+  sed -e "s/component/${COMPONENT}/" -e "s/PRIVATE_IP/${PRIVATE_IP}" components/dnsrecord.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${Zone_ID} --change-batch file:/tmp/record.json | jq
   StatCheck $?
 }
