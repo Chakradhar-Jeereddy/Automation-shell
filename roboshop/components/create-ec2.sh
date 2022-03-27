836810da233cf9cf4eed526feaafb6e08ae918fa
chakra <chakradhar06@gmail.com>
2022-03-27 18:47:21 -0400

    Preparing mongodb

--- roboshop/components/create-ec2.sh
+++ roboshop/components/create-ec2.sh
@@ -30,7 +30,7 @@ create_ec2() {
   StatCheck $?
   Print "Create DNS record"
   sed -e "s/component/${COMPONENT}/" -e "s/PRIVATE_IP/${PRIVATE_IP}/" components/dnsrecord.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id ${Zone_ID} --change-batch file:///tmp/record.json | jq
+  aws route53 change-resource-record-sets --hosted-zone-id ${Zone_ID} --change-batch file:///tmp/record.json | jq &>>${LOG_FILE}
   StatCheck $?
 }
 
