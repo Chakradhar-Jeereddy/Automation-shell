22f89567362a643c8098e45977e4f5eef10cf0f9
chakra <chakradhar06@gmail.com>
2022-03-27 18:26:48 -0400

    Preparing mongodb

--- roboshop/components/create-ec2.sh
+++ roboshop/components/create-ec2.sh
@@ -22,11 +22,11 @@ StatCheck $?
 create_ec2() {
   Print "Create spot instance"
   PRIVATE_IP=(aws ec2 run-instances \
-  --image-id $AMI_ID \
-  --instance-type t2.micro \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]" \
-  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-  --security-group-ids $SG_ID | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+    --image-id $AMI_ID \
+    --instance-type t2.micro \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]" \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+    --security-group-ids $SG_ID | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
   sed -e "s/component/${COMPONENT}/" -e "s/PRIVATE_IP/${PRIVATE_IP}" dnsrecord.json &>/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${Zone_ID} --change-batch file:/tmp/record.json | jq
   StatCheck $?
