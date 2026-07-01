1c7a0728c731f7c0f90c2855cfde40d816c9d993
chakradhar06 <chakradhar06@gmail.com>
2026-07-01 19:57:35 -0400

    command variables

---
+++ shell_pactice/05-cmd-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#command is ()
+
+DATE=$(date +%F)
+START_TIME=$(date +s)
+sleep 10
+END_TIME=$(date +s)
+
+#For calucation use $((expression)) syntax. Here expression is the mathematical expression to be evaluated.
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo "Script executed in: $TOTAL_TIME seconds"
