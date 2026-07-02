c92f4f33be80e2f3b994d40a01de6bf12d6fa886
chakradhar06 <chakradhar06@gmail.com>
2026-07-01 22:24:10 -0400

    loops

---
+++ shell_pactice/14-loops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for i in {1..10}
+do  
+ echo "Welcome $i times"
+done
+
+# Use cases
+#1 - No performance imporvement
+#2 - Efforts are reduced
+#3 - Easy to maintain
+#4 - Less code duplication
+#5 - Dynamic workloads and code generation
+
