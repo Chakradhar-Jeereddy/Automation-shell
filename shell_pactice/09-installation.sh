2fea5db3e08295a4764ab5dac3e6932eb06cb5e7
chakradhar06 <chakradhar06@gmail.com>
2026-07-01 21:13:46 -0400

    installation

--- shell_pactice/09-installation.sh
+++ shell_pactice/09-installation.sh
@@ -2,9 +2,9 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo "Please run this script with root privileges"
-    exit 1
+    exit 1 #failure if other then 0
 fi
 
 dnf install mysql -y
