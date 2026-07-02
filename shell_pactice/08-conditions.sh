00e552ceba4364b903bbe6fac3f128544709b199
chakradhar06 <chakradhar06@gmail.com>
2026-07-01 20:57:03 -0400

    conditions

--- shell_pactice/08-conditions.sh
+++ shell_pactice/08-conditions.sh
@@ -7,11 +7,11 @@
 number=$1
 
 if [ $number -lt 10 ]; then
-   echo "Given number is less than 10"
+   echo "Given $number is less than 10"
 elif [ $number -eq 10 ]; then
-    echo "Given number is equal to 10"
+    echo "Given $number is equal to 10"
 else
-   echo "Geven number is greater than 10"
+   echo "Geven $number is greater than 10"
 fi
 
 # -gt
