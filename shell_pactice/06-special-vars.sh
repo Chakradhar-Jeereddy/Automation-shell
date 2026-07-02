61f951c7676eaaad389cd9b396ba1c17be5603c2
chakradhar06 <chakradhar06@gmail.com>
2026-07-01 20:26:36 -0400

    special variables

---
+++ shell_pactice/06-special-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "ALL variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "Total number of variables passed to the script: $#"
+echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
+sleep 50 &
+echo "PID of last background command: $!"
