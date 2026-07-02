600646e55fd228007f2fc6bae40afb84f370ff6a
chakradhar06 <chakradhar06@gmail.com>
2026-07-01 21:21:43 -0400

    functions

---
+++ shell_pactice/10-functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "Please run this script with root privileges"
+    exit 1 #failure if other then 0
+fi
+
+validate() {
+    if [ $1 -ne 0 ]" then
+        echo "Installing $2 is failed"
+        exit 1
+    else
+        echo "Installing $2 is Success"
+    fi
+}
+
+dnf install mysql -y
+validate $? mysql
+
+dnf install nginx -y
+validate $> nginx
