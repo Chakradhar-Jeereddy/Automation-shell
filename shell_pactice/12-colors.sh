68866228a84dc08e79cd4dac28a34a2cec514329
chakradhar06 <chakradhar06@gmail.com>
2026-07-01 21:44:17 -0400

    colors

---
+++ shell_pactice/12-colors.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERUD -ne 0 ]; then
+    echo -e "${R}Please run this script with root privileges${N}"
+    exit 1 #failure if other then 0
+fi
+
+validate() {
+    if [ $1 -ne 0 ]; then
+        echo -e "${R}Installing $2 is failed${N}"
+        exit 1
+    else
+        echo -e "${G}Installing $2 is Success${N}"
+    fi
+}
+
+dnf install mysql -y
+validate $? mysql
+
+dnf install nginx -y
+validate $? nginx
